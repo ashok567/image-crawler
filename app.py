41a9c404f9ef2b5b96ed67782936ae8d212809a6
Ashok Thakur <iashok180@gmail.com>
2019-07-22 02:52:27 +0530

    UI issues fixed

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
     
+class DataHandler(tornado.web.RequestHandler):
     def post(self):
-        self.set_header("Content-Type", "text/plain")
+        self.set_header("Content-Type", "application/json")
         url = self.get_body_argument("url")
         scraper.download_image(url)
+        self.write({'response': "ok"})
 
     
 settings = dict(
@@ -24,6 +26,7 @@ def make_app():
     return tornado.web.Application(
     [
         (r'/', MainHandler),
+        (r'/data', DataHandler),
         (r'/(.*)', tornado.web.StaticFileHandler, {"path": ""}),
     ],**settings)
 
